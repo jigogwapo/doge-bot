f9695765f540afde84ac181b2aba2f9987c75aef
Jigs <jigogwapo16@gmail.com>
2021-01-15 14:32:50 +0800

    added auto-delete on voicechat channel

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-
+import asyncio
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -7,6 +7,7 @@ class Admin(commands.Cog):
     starden_server_id = 758361018233126932
     starden_anonchannel_id = 789854981981077514
     starden_testchannel_id = 780701253280727040
+    starden_voicechatchannel_id = 798943215008088155
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -21,6 +22,11 @@ class Admin(commands.Cog):
                 anon_message = message.content[6:]
                 await starden_anonchannel.send(f'**anon**: {anon_message}')
                 await message.channel.send(f'anon message successfully sent. you can now delete your DM.')
+
+        if message.channel.id == Admin.starden_anonchannel_id:
+            await asyncio.sleep(20)
+            await message.reply('the message will be deleted after 20 seconds.', delete_after=20)
+            await message.delete()
 
 def setup(bot):
     bot.add_cog(Admin(bot))
