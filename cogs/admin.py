50e8b8ff8e793e9421056092fe0fa2c9438e0f9e
Jigs <jigogwapo16@gmail.com>
2021-01-15 15:01:03 +0800

    removed unneeded line

--- cogs/admin.py
+++ cogs/admin.py
@@ -24,7 +24,6 @@ class Admin(commands.Cog):
                 await message.channel.send(f'anon message successfully sent. you can now delete your DM.')
 
         if message.channel.id == Admin.starden_testchannel_id and not message.author.bot:
-            content = message.content
             await asyncio.sleep(9)
             await message.reply(f'Deleting...', delete_after=1)
             await asyncio.sleep(1)
