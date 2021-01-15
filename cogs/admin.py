d398c03a81f828ee90c2419bd6772baedb06affc
Jigs <jigogwapo16@gmail.com>
2021-01-15 15:44:11 +0800

    added another check for bot

--- cogs/admin.py
+++ cogs/admin.py
@@ -24,7 +24,8 @@ class Admin(commands.Cog):
                 await message.channel.send(f'anon message successfully sent. you can now delete your DM.')
 
         if message.channel.id == Admin.starden_testchannel_id:
-            await message.reply(f'Message will be deleted in 10 seconds.', delete_after=1)
+            if not message.author.bot:
+                await message.reply(f'Message will be deleted in 10 seconds.', delete_after=1)
             await asyncio.sleep(9)
             if not message.author.bot:
                 await message.reply(f'Deleting...', delete_after=1)
