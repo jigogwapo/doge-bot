17d4a3ac27344c293c8d2175761335a65c1891f3
Jigs <jigogwapo16@gmail.com>
2021-01-19 15:44:55 +0800

    added quote categories

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -1,14 +1,33 @@
 from discord.ext import commands
-from helpers.quotes_helpers import get_random_quote
+from helpers.quotes_helpers import get_random_quote, get_tag_list, get_random_quote_with_tag
 
 class Quotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(brief='display a random quote')
-    async def quote(self, ctx):
-        quote = get_random_quote()
-        await ctx.send(f'> *{quote["content"]}* - {quote["author"]}')
+    async def quote(self, ctx, category : str = None):
+        if category is None:
+            quote = get_random_quote()
+        else:
+            category_list = get_tag_list()
+            if category in category_list:
+                quote = get_random_quote_with_tag(category)
+            else:
+                quote = None
+
+        if quote is None:
+            await ctx.send('No such category. Check list of categories with `*qcats`')
+        else:
+            await ctx.send(f'> *{quote["content"]}* - {quote["author"]}')
+
+    @commands.command(brief='display a list of quote categories')
+    async def qcats(self, ctx):
+        category_list = get_tag_list()
+        content = 'Here\'s a list of quote categories:'
+        for category in category_list:
+            content += f'\n{category}'
+        await ctx.send(f'```{content}```')
 
 def setup(bot):
     bot.add_cog(Quotes(bot))
--- helpers/quotes_helpers.py
+++ helpers/quotes_helpers.py
@@ -9,3 +9,16 @@ def get_random_quote():
     quote['author'] = quote_json['author']
 
     return quote
+
+def get_tag_list():
+    tag_json = requests.get(f'{base_uri}/tags').json()
+    tag_list =[tag.name for tag in tag_json]
+    return tag_list
+
+def get_random_quote_with_tag(tag):
+    quote_json = requests.get(f'{base_uri}/random?tags={tag}').json()
+    quote = {}
+    quote['content'] = quote_json['content']
+    quote['author'] = quote_json['author']
+
+    return quote
