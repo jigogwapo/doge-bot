f6ee3ca7b564f35ca03753e302bec5cd76e4c8f5
Jigs <jigogwapo16@gmail.com>
2021-01-25 22:34:41 +0800

    add doge pic to quote

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -15,6 +15,7 @@ class Quotes(commands.Cog):
         if category is None:
             quote = get_random_quote()
             embed = Embed(title=quote['content'], description=quote['author'])
+            embed.set_thumbnail(url='https://i.imgur.com/HeGEEbu.jpg')
             embed.set_footer(text=Quotes.quotable_credits)
             await ctx.send(embed=embed)
         else:
@@ -23,11 +24,13 @@ class Quotes(commands.Cog):
                 if category == 'anime':
                     quote = get_random_anime_quote()
                     embed = Embed(title=quote['content'], description=f'{quote["character"]} ({quote["anime"]})')
+                    embed.set_thumbnail(url='https://i.imgur.com/HeGEEbu.jpg')
                     embed.set_footer(text=Quotes.animechan_credits)
                     await ctx.send(embed=embed)
                 else:
                     quote = get_random_quote_with_tag(category)
                     embed = Embed(title=quote['content'], description=quote['author'])
+                    embed.set_thumbnail(url='https://i.imgur.com/HeGEEbu.jpg')
                     embed.set_footer(text=Quotes.quotable_credits)
                     await ctx.send(embed=embed)
             else:
