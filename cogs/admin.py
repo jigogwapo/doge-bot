6daf8e327242a1845d02ca9d841a1d6586919399
Jigs <jigogwapo16@gmail.com>
2021-02-19 09:09:42 +0800

    fix status

--- cogs/admin.py
+++ cogs/admin.py
@@ -41,7 +41,7 @@ class Admin(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Bot connected as {self.bot.user}')
-        await self.bot.change_presence(activity=discord.Streaming(name="Playing till I reach Valhalla", game="Valheim", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", platform='YouTube'))
+        await self.bot.change_presence(activity=discord.Streaming(platform="YouTube", name="Playing Valheim", game="Valheim", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
 
 
     @commands.Cog.listener()
