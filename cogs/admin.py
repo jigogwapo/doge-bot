d7a5a8d79b0b755392bd19174c5e53a7397db384
Jigs <jigogwapo16@gmail.com>
2021-02-19 10:13:00 +0800

    discord streaming presence bug fix

--- cogs/admin.py
+++ cogs/admin.py
@@ -41,7 +41,7 @@ class Admin(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Bot connected as {self.bot.user}')
-        await self.bot.change_presence(activity=discord.Streaming(name='YouTube', details="Valheim", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+        await self.bot.change_presence(activity=discord.Streaming(name='YouTube', url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", details="Valheim", state="Valheim"))
 
     @commands.Cog.listener()
     async def on_message(self, message):
